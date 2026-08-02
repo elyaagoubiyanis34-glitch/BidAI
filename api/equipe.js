ebc5f21a6466ad2f8ed6088f9e24e5d016515b53
BidRay <elyaagoubi.yanis34@gmail.com>
2026-08-02 21:00:19 +0200

    Update equipe.js

--- api/equipe.js
+++ api/equipe.js
@@ -28,7 +28,8 @@ async function resteAuMoinsUn(org_id, perm, exclu_user_id, futureValeur) {
 }
 
 module.exports = async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', 'https://getbidray.com');
+  res.setHeader('Vary', 'Origin');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   if (req.method === 'OPTIONS') return res.status(200).end();
